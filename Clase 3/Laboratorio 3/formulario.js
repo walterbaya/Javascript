b82794a74de2f186c5cfc770df031860d04def76
walterbaya <walterbaya@yahoo.com>
2021-01-20 00:49:14 -0300

    corrigiendo el ejercicio de mail se estaria casi finalizando la parte 1 de la tarea

--- Clase 3/Laboratorio 3/formulario.js
+++ Clase 3/Laboratorio 3/formulario.js
@@ -1,12 +1,12 @@
 let inputs = document.querySelectorAll("input,textarea");
 
-inputs.forEach(function (input) {
-    input.addEventListener("focus", function (e) {
+inputs.forEach(function(input) {
+    input.addEventListener("focus", function(e) {
         e.target.parentNode.children[0].classList.add("subir")
         e.target.parentNode.classList.add("animacionBorde")
     });
 
-    input.addEventListener("blur", function (e) {
+    input.addEventListener("blur", function(e) {
         if (e.target.value.length <= 0) {
             e.target.parentNode.children[0].classList.remove("subir")
         }
@@ -24,9 +24,9 @@ let message = document.getElementById("mensaje");
 let submit = document.querySelector("form");
 
 
-let getWords = (s) => {
+let getWords = (s, a) => {
     let res = [];
-    let aux = s.split(" ");
+    let aux = s.split("" + a);
     aux.forEach(w => {
         if (w != "") {
             res.push(w);
@@ -40,22 +40,19 @@ let validateNames = (e, l) => {
     let palabra1;
     let palabra2;
     let s = n.value;
-    let words = getWords(s);
+    let words = getWords(s, " ");
     switch (words.length) {
         case 0:
             n.className = "error";
             n.setCustomValidity("El " + e.target.id + " No puede estar vacío");
-            alert("El " + e.target.id + " No puede estar vacío");
-
+            alert("El " + e.target.id + " No puede estar vacío   ");
             break;
         case 1:
             palabra1 = words[0];
             if (palabra1.length < l) {
                 n.className = "error";
                 n.setCustomValidity("La palabra debe tener tamaño de al menos: " + l);
-                alert("La palabra debe tener tamaño de al menos: " + l);
-            }
-            else {
+            } else {
                 n.className = "success";
                 n.setCustomValidity("");
             }
@@ -66,9 +63,7 @@ let validateNames = (e, l) => {
             if (palabra1.length < l || palabra2.length < l) {
                 n.className = "error";
                 n.setCustomValidity("Las palabras deben tener tamaño de al menos: " + l);
-                alert("Las palabras deben tener tamaño de al menos: " + l);
-            }
-            else {
+            } else {
                 n.className = "success";
                 n.setCustomValidity("");
             }
@@ -89,15 +84,67 @@ nombre.addEventListener("blur", e => validateNames(e, 2));
 apellido.addEventListener("blur", e => validateNames(e, 2));
 
 //1.3)email : El email debe respetar el formato nombre@dominio.com pudiendo contener el nombre caracteres especiales como -_. 
+
+validarMail = (variable, e) => {
+    let n = e.target;
+    let error = false;
+    let dosPartes = getWords(n.value, "@");
+    if (dosPartes.lenght == 2) {
+        if (dosPartes[1].endsWith(".com")) {
+            console.log(dosPartes);
+            if (dosPartes[1].lenght > 3) {
+                n.setCustomValidity("Error, el " + variable + " debe respetar el formato nombre@dominio.com");
+                n.className = "error";
+                alert("Error, el " + variable + " debe respetar el formato nombre@dominio.com");
+                error = true;
+            } else {
+                n.setCustomValidity("");
+                n.className = "success";
+            }
+        } else {
+            n.setCustomValidity("Error, el " + variable + " debe finalizar en .com");
+            n.className = "error";
+            alert("Error, el email debe finalizar en .com");
+            error = true;
+        }
+    } else {
+        n.setCustomValidity("");
+        n.className = "error";
+        alert("Error, el " + variable + " debe respetar el formato nombre@dominio.com");
+        error = true;
+    }
+}
+
+email.addEventListener("blur", e => {
+    validarMail("email", e);
+});
+
+
 //1.4)reemail : Su valor debe ser identico al de email
+
+reemail.addEventListener("blur", e => {
+    let error = validarMail("mail", e);
+    if (error == false) {
+        if (e.target.value == email.value) {
+
+        } else {
+            e.target.className = "error";
+            alert("Error, el reemail debe ser igual al email.");
+            error = true;
+        }
+    }
+});
+
+
+
+
 //1.5)contraseña : La contraseña debe tener como mínimo 6 letras sin espacios y los caracteres especiales que se pueden usar son ?!_-.
 
 //se recibe un codigo unicode
 isValidCaracterForPassword = (l) => {
     if ((97 <= l && l <= 122) || (65 <= l && l <= 90) || l == 63 || l == 33 || l == 95) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
@@ -117,9 +164,8 @@ password.addEventListener("blur", (e) => {
         if (!allCaractersValid(e.target.value)) {
             e.target.className = "error";
             e.target.setCustomValidity("Las palabras deben tener tamaño de al menos: " + 6);
-            alert("Hay caracteres invalidos, recuerde que solo se pueden usar letras minusculas y mayusculas y ? ! - _ ");
-        }
-        else {
+            alert("Hay caracteres invalidos, recuerde que solo se pueden usar letras minusculas y mayusculas y ? ! - _  para el password");
+        } else {
             e.target.className = "success";
             e.target.setCustomValidity("");
         }
@@ -127,7 +173,29 @@ password.addEventListener("blur", (e) => {
 })
 
 //1.6)titulo : El titulo puede ser opcional. Si el mismo está faltante, se debe usar el string "Post Anonimo"
+
+titulo.addEventListener("blur", e => {
+    if (e.target.value == "") {
+        e.target.parentNode.children[0].classList.add("subir");
+        e.target.parentNode.classList.add("animacionBorde");
+        e.target.value = "Post Anonimo";
+    }
+});
+
+
 //1.7)mensaje : El mensaje debe tener como mínimo una letra. Puede ser cualquier caracter siempre y cuando el mismo no se imprima en pantalla sin ser escapado, de lo contrario podríamos tener un ataque XSS.
+
+let valorMensaje;
+mensaje.addEventListener("blur", e => {
+
+    if (e.target.value == "") {
+
+    } else {
+        valorMensaje = escape(e.target.value);
+    }
+});
+
+
 
 
 
@@ -139,6 +207,10 @@ password.addEventListener("blur", (e) => {
 
 submit.addEventListener("submit", e => {
     //cracion de un evento blur y su lanzamiento 
+    const event = new Event("blur");
+    nombre.dispatchEvent(event);
+    apellido.dispatchEvent(event);
+    password.dispatchEvent(event);
 
 });
 
@@ -155,5 +227,3 @@ submit.addEventListener("submit", e => {
 
 //BONUS :
 //5)Realizar las mismas validaciones usando RegExp.
-
-
