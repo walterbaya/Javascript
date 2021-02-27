eb463a2650f7395157e19a64e60d2d51a812b131
walterbaya <walterbaya@yahoo.com>
2021-02-26 21:18:12 -0300

    se sube laboratorio primeros 3 ejerciciso

---
+++ Clase 6/Laboratorio/app.js
@@ -0,0 +1,123 @@
+//Seleccion de elementos globales
+let menu = document.querySelector(".material-icons")
+let drawer = document.querySelector("#drawer")
+let links = document.querySelectorAll("a")
+let loader = document.querySelector("img")
+
+/*
+* EVENTOS
+**/
+
+//Carga del Home
+if (loader.complete) {
+    ajax("home.html","get",function(data){
+        render("main",data)
+    },true)
+}else{
+    loader.addEventListener("load",function(e){
+        ajax("home.html","get",function(data){
+            render("main",data)
+        },true)
+    })
+}
+
+//Pedidos de ajax
+links.forEach(function(link){
+    link.addEventListener("click",function(e){
+        e.preventDefault();
+        drawer.style.left = ''
+        ajax(e.target.innerText+".html","get",function(data){
+            render("main",data)
+            portfolioLoad()
+        },true)
+    })
+})
+
+
+
+
+//Animacion del drawer
+menu.addEventListener("click",function(e){
+    if (drawer.style.left) {
+        drawer.style.left = ''
+    }else{
+        drawer.style.left = '0px'
+    }
+})
+//Precarga de contenido en historial
+window.addEventListener("popstate",function(e){
+    console.log(e.state)
+    render("main",e.state.template)
+})
+
+//Animaciones de los input y textarea labels
+document.body.addEventListener("focusin",function(e){
+    if(e.target.tagName.toLowerCase() == "input" || e.target.tagName.toLowerCase() == "textarea"){
+        e.target.parentNode.children[0].classList.add("subir")
+        e.target.parentNode.classList.add("animacionBorde")
+    }
+})
+document.body.addEventListener("focusout",function(e){
+    if(e.target.tagName.toLowerCase() == "input" || e.target.tagName.toLowerCase() == "textarea"){
+        if(e.target.value.length <= 0){
+            e.target.parentNode.children[0].classList.remove("subir")
+        }
+        e.target.parentNode.classList.remove("animacionBorde")
+    }
+})
+
+
+//Eventos dinamicos
+document.addEventListener("click",function(e){
+    let elemento;
+    if(e.target.tagName.toLowerCase() == "button"){
+        elemento = e.target
+    }else{
+        if(e.target.tagName.toLowerCase() == "article"){
+            elemento = e.target
+        }
+        if(e.target.tagName.toLowerCase() == "footer" || e.target.tagName.toLowerCase() == "img"){
+            if(e.target.parentNode.parentNode.tagName.toLowerCase() == "article"){
+                elemento = e.target.parentNode.parentNode
+            }
+        }
+    }
+    if(elemento){
+        switch(elemento.id){
+            case "listado":
+                ajax("listado.html","get",function(data){
+                    render("main",data)
+                },true)
+            break;
+            case "traduccion":
+                ajax("traduccion.html","get",function(data){
+                    render("main",data)
+                },true)
+            break;
+            case "usuarios":
+                ajax("http://jsonplaceholder.typicode.com/users","get",function(data){
+                    let usuarios = JSON.parse(data)
+                    let fragmento = document.createDocumentFragment()
+                    usuarios.forEach(function(usuario){
+                        let li = document.createElement("li")
+                        li.innerText = usuario.name
+                        fragmento.appendChild(li)
+                    })
+                    document.querySelector("#listado-usuarios").appendChild(fragmento)
+                },false)
+            break;
+            case "dropzone-article":
+                ajax("dropzone.html","get",function(data){
+                    render("main",data)
+                },true)
+            break;
+            case "download-article":
+                ajax("download.html","get",function(data){
+                    render("main",data)
+                    crearLinks()
+                },true)
+            break;
+        }
+    }
+})
+
---
+++ Clase 6/Laboratorio/funciones.js
@@ -0,0 +1,49 @@
+//Funciones Utilitarias
+/*
+ *@param url       String   : La url donde hacemos el pedido
+ *@param metodo    String   : El metodo HTTP del pedido
+ *@param callback  Function : La funcion a ejecutar en el evento load 
+ */
+function ajax(url, metodo, callback, historial, tipoArchivo) {
+    let xhr = new XMLHttpRequest
+    if (tipoArchivo) {
+        xhr.responseType = "blob"
+    }
+    xhr.open(metodo, url)
+    xhr.addEventListener("load", function() {
+        if (xhr.status == 200) {
+            if (historial) {
+                window.history.pushState({
+                    url: url.split(".")[0],
+                    template: xhr.response
+                }, "", url.split(".")[0])
+            }
+            callback(xhr.response)
+        }
+    })
+    xhr.send()
+}
+/*
+ *@param selector    String : El selector donde se le hara render a la informacion
+ *@param data        String : La informacion para mostrar
+ */
+function render(selector, data) {
+    document.querySelector(selector).innerHTML = data
+}
+/*
+ * Es la funcion que se encarga de determinar si la pagina que se acaba de cargar es la de portfolio
+ */
+function portfolioLoad() {
+    let articulos = document.querySelectorAll("article")
+    if (articulos.length) {
+        articulos.forEach(function(articulo) {
+            ajax("https://dog.ceo/api/breeds/image/random", "get", function(data) {
+                let data_parseada = JSON.parse(data)
+                let url = data_parseada.message
+                let img = document.createElement("img")
+                img.src = url
+                articulo.children[0].appendChild(img)
+            }, false)
+        })
+    }
+}
