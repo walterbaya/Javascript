e76950e9d7b256ee94fc73bfb70c5e3f1a01e022
walterbaya <56239454+walterbaya@users.noreply.github.com>
2021-07-13 13:23:47 -0300

    se sube node js

---
+++ Clase 12/Teorica/Nodejs/index.js
@@ -0,0 +1,38 @@
+/*
+Node js permite correr codigo js, es un entorno de ejecucion
+node js es javascript ejecutado en el sistema operativo, es decir
+del lado del servidor.
+Un modulo es un archivo, ya que tecnicamente node a medida que 
+va leyendo los archivos node implementa el patron modulo, encapsulando
+nuestras clases.
+Los modulos nativos son como las web apis pero del lado del 
+servidor.
+Hay varios modulos nativos de nodeJS, se pueden ver en la
+pagina.
+
+NPM es un sistema que permite controlar determinados paquetes/librerias
+dentro de node, node package manager.
+Es uno de los repositorios mas grandes que hay en el mundo.
+*/
+
+//Creacion de servidor en NodeJS
+
+//npm init y responder el cuestionario.
+/*package.json sirve para que en el caso en que desarrollemos algo
+programa poder publicarlo en npm, si queremos tener algun paquete podemos utilizar
+npm install  --save (nombre del paquete) para instalar un paquete, para que lo guarde dentro del archivo configuraicon
+como una dependencia real del proyecto.
+
+ecmascript se va actualizando por año.
+babel: es un compilador de codigo de javascript.
+sirve para traducir nuestro codigo de javascript moderno a codigo
+de javascript mas compatible con los navegadores anteriores.
+
+webpack compila codigo y nos sirve para varios formatos, de manera que permite
+juntar y minificar los archivos en un unico archivo fuente, para que el cliente solo haga un
+solo pedido y no varios.
+
+COMO HACER UN REJUNTE DE LOS ARCHIVOS
+
+install --save-dev webpack webpack--cli
+*/
